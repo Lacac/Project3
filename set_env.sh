953cffaea127ea36618bd9f9909a9924c2386852
Laclac <ngochths150417@fpt.eedu.vn>
2022-10-10 19:49:04 +0700

    project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=ngocht9
+export POSTGRES_PASSWORD=Nn432168
+export POSTGRES_HOST=module3-project.cuablfqqogkp.ap-southeast-1.rds.amazonawzonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=module3-project
+export AWS_REGION=ap-southeast-1
+export AWS_PROFILE=
+export JWT_SECRET=testing
+export URL=http://localhost:8100
